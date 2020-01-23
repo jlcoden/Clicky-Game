d9558d21ae58368a34561d1008e7b6a8b46a913e
Josh Cosson <joshlcosson@gmail.com>
2020-01-22 23:33:45 -0800

    Updates

---
+++ precache-manifest.10b2ebd78f7107526e338974d91aebc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5671874dccd20cdceea954c0984e200",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "f10e5886da77f1980e77",
+    "url": "/Clicky-Game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6191607efd68b37909ee",
+    "url": "/Clicky-Game/static/css/main.7a8b6a6d.chunk.css"
+  },
+  {
+    "revision": "f10e5886da77f1980e77",
+    "url": "/Clicky-Game/static/js/2.773a2e17.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.773a2e17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6191607efd68b37909ee",
+    "url": "/Clicky-Game/static/js/main.155e241e.chunk.js"
+  },
+  {
+    "revision": "ed33e96b23e5ad0b910f",
+    "url": "/Clicky-Game/static/js/runtime-main.da6617cf.js"
+  }
+]);
